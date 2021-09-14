66c446850f062f074dbccd27db5e42618b62d373
laryssa15s <lms16@aluno.ifal.edu.br>
2021-09-14 21:20:22 +0000

    mostrar o parâmetro invalido

--- calc-api-924/src/main/java/app/Soma.java
+++ calc-api-924/src/main/java/app/Soma.java
@@ -5,15 +5,31 @@ import io.jooby.exception.*;
 public class Soma{
     @Path ( "/soma/{x}/{y}")
     @GET 
-    public String rotaSoma(@PathParam String x, @PathParam String y){
+    public String rotasoma(@PathParam String x , @PathParam String y) {
         try {
             double sum = Double.parseDouble(x) + Double.parseDouble(y);
 
             return String.format("%.2f", sum);
-        } 
-        catch (NumberFormatException nfe) {
-           throw new BadRequestException(String.format("Contém parâmetro inválido"));
+        } catch (NumberFormatException nfe) {
+
+            if(isInvalid(x) && isInvalid(y) == false){
+                throw new BadRequestException(String.format("O parâmetro não é válido: %s", x));
+            } else if(isInvalid(x) == false && isInvalid(y)){
+                throw new BadRequestException(String.format("O parâmetro não é válidos: %s", y));
+            } 
+
+            throw new BadRequestException(String.format("Os parâmetros não são válidos: %s e %s.", x, y));
         }
-        
+    }
+
+    private Boolean isInvalid(String numero){
+        try{
+            Double.parseDouble(numero);
+            return false;
+        }catch(NumberFormatException nfe){
+            return true;
+        }
     }
 }
+
+
